4ecbfaad2aa7fd8ec1ebd3d6169970ca35494865
Elias <elikapeluschnik@gmail.com>
2021-11-23 20:54:25 -0500

    commit tree a7edf2412b997b32121591b99311ab9e84c16321

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,27 @@
 class Dog
 
+    def name= (name)
+        @name = name
+    end
+
+    def breed= (breed)
+        @breed = breed
+    end
+
+    def name
+        @name
+    end
+
+    def breed
+        @breed
+    end
+
 end
+
+# d1 = Dog.new('Mastiff')
+# d2 = Dog.new('Chihuahua')
+# d3 = Dog.new('Corgi')
+# d5 = Dog.new('Beagle')
+# d6 = Dog.new('French Bulldog')
+# d7 = Dog.new('pug')
+# d8 = Dog.new('Pointer')
